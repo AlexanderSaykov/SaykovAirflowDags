73acfd7a21a75bb88457d42bdf750ab62fea0aa8
AlexanderSaykov <79256653+AlexanderSaykov@users.noreply.github.com>
2022-08-22 11:26:17 +0300

    Update update_project.py

--- update_project.py
+++ update_project.py
@@ -18,68 +18,79 @@ with DAG(
     update_departments = PostgresOperator(
         task_id='update_departments',
         postgres_conn_id='bi',
-        sql="""call project.departments_insert()"""
+        sql="""call project.departments_insert()""",
+        autocommit = True
     )
 
     update_resource_id_mapping = PostgresOperator(
         task_id='update_resource_id_mapping',
         postgres_conn_id='bi',
-        sql="""call project.resource_id_mapping_insert()"""
+        sql="""call project.resource_id_mapping_insert()""",
+         autocommit = True
     )
 
     update_resources = PostgresOperator(
         task_id='update_resources',
         postgres_conn_id='bi',
-        sql="""call project.resources_insert()"""
+        sql="""call project.resources_insert()""",
+         autocommit = True
     )
 
     update_rates = PostgresOperator(
         task_id='update_rates',
         postgres_conn_id='bi',
-        sql="""project.rates_insert()"""
+        sql="""project.rates_insert()""",
+         autocommit = True
     )
 
     update_projects = PostgresOperator(
         task_id='update_projects',
         postgres_conn_id='bi',
-        sql="""call project.projects_insert()"""
+        sql="""call project.projects_insert()""",
+         autocommit = True
     )
 
     update_tasks = PostgresOperator(
         task_id='update_tasks',
         postgres_conn_id='bi',
-        sql="""call project.tasks_insert()"""
+        sql="""call project.tasks_insert()""",
+         autocommit = True
     )
 
     update_assignments = PostgresOperator(
         task_id='update_assignments',
         postgres_conn_id='bi',
-        sql="""call project.assignments_insert()"""
+        sql="""call project.assignments_insert()""",
+         autocommit = True
     )
 
     update_timesheet_status = PostgresOperator(
         task_id='update_timesheet_status',
         postgres_conn_id='bi',
-        sql="""call timesheet_status_insert()"""
+        sql="""call timesheet_status_insert()""",
+         autocommit = True
 
     )
 
     update_timesheet_line_status = PostgresOperator(
         task_id='update_timesheet_line_status',
         postgres_conn_id='bi',
-        sql="""call project.timesheet_line_status_insert()"""
+        sql="""call project.timesheet_line_status_insert()""",
+         autocommit = True
     )
 
     update_timesheet_lines = PostgresOperator(
         task_id='update_timesheet_lines',
         postgres_conn_id='bi',
-        sql="""call project.timesheet_lines_insert()"""
+        sql="""call project.timesheet_lines_insert()""",
+         autocommit = True
     )
 
     update_facts = PostgresOperator(
         task_id='update_facts',
         postgres_conn_id='bi',
-        sql="""call project.facts_insert()"""
+        sql="""call project.facts_insert()""",
+         autocommit = True
     )
 
 update_departments >> update_resource_id_mapping >> update_resources >> update_rates >> update_projects >> update_tasks >> update_assignments >> update_timesheet_status >> update_timesheet_line_status >> update_timesheet_lines >> update_facts
